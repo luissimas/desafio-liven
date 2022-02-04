6db6a69d592c5ac8cae1dbde58bf1c3befcfe259
Luís Simas <luissimas@protonmail.com>
2022-02-04 13:15:06 -0300

    More user-update tests

--- src/domain/use-cases/user/update-user/update-user.spec.ts
+++ src/domain/use-cases/user/update-user/update-user.spec.ts
@@ -2,6 +2,7 @@ import { User } from '@entities/user'
 import { MemoryUserRepository } from '@infrastructure/repositories/user/memory-user-repository'
 import { UpdateUserUseCase } from '.'
 import { IUpdateUserDTO } from './update-user-dto'
+import { UserNotFound } from '@errors'
 import { compare } from 'bcrypt'
 
 const repo = new MemoryUserRepository()
@@ -44,5 +45,105 @@ describe('Update user use case', () => {
         expect(match).toBeTruthy()
       }
     }
+  })
+
+  it('Should be able to update only user name', async () => {
+    const oldUser = new User({
+      name: 'Jorge',
+      email: 'jorge@gmail.com',
+      password: 'banana1928',
+    })
+
+    await repo.save(oldUser)
+
+    const data: IUpdateUserDTO = {
+      id: oldUser.id,
+      name: 'Jorge Silva',
+    }
+
+    await useCase.execute(data)
+
+    const user = await repo.findById(oldUser.id)
+
+    expect(user).toBeDefined()
+
+    if (user) {
+      expect(user.id).toEqual(oldUser.id)
+      expect(user.name).toEqual(data.name)
+      expect(user.email).toEqual(oldUser.email)
+
+      if (data.password) {
+        const match = await compare(oldUser.password, user.password)
+        expect(match).toBeTruthy()
+      }
+    }
+  })
+
+  it('Should be able to update only user email', async () => {
+    const oldUser = new User({
+      name: 'Jorge',
+      email: 'jorge@gmail.com',
+      password: 'banana1928',
+    })
+
+    await repo.save(oldUser)
+
+    const data: IUpdateUserDTO = {
+      id: oldUser.id,
+      email: 'outrodosjorges@gmail.com',
+    }
+
+    await useCase.execute(data)
+
+    const user = await repo.findById(oldUser.id)
+
+    expect(user).toBeDefined()
+
+    if (user) {
+      expect(user.id).toEqual(oldUser.id)
+      expect(user.name).toEqual(oldUser.name)
+      expect(user.email).toEqual(data.email)
+
+      if (data.password) {
+        const match = await compare(oldUser.password, user.password)
+        expect(match).toBeTruthy()
+      }
+    }
+  })
+
+  it('Should be able to update only user password', async () => {
+    const oldUser = new User({
+      name: 'Jorge',
+      email: 'jorge@gmail.com',
+      password: 'banana1928',
+    })
+
+    await repo.save(oldUser)
+
+    const data: IUpdateUserDTO = {
+      id: oldUser.id,
+      password: 'maça182',
+    }
+
+    await useCase.execute(data)
+
+    const user = await repo.findById(oldUser.id)
+
+    expect(user).toBeDefined()
+
+    if (user) {
+      expect(user.id).toEqual(oldUser.id)
+      expect(user.name).toEqual(oldUser.name)
+      expect(user.email).toEqual(oldUser.email)
+
+      if (data.password) {
+        const match = await compare(data.password, user.password)
+        expect(match).toBeTruthy()
+      }
+    }
+  })
+
+  it('Should not be able to delete an inexistent user', async () => {
+    expect(async () => await useCase.execute({ id: 'alskdjf' })).rejects.toThrow(UserNotFound)
   })
 })
